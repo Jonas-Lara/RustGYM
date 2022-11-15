b56b2ded4dd9a27e9500e7061df1ee83178e5729
Jonas Lara <61485427+Jonas-Lara@users.noreply.github.com>
2022-11-14 19:12:57 -0600

    Update 01-TwoSums.cpp

--- Algoritmos/01-TwoSums.cpp
+++ Algoritmos/01-TwoSums.cpp
@@ -2,29 +2,38 @@
 #include <unordered_map>
 #include <vector>
 
-using namespace std;
-
-vector<int> twoSum(vector<int>& A, int target) 
+std::vector<int> twoSum(std::vector<int>& array, int target)
 {
-   unordered_map<int, int> m;
-	for (int i = 0; i < A.size(); ++i) 
-	{
-		int t = target - A[i];
-		if (m.count(t)) return { m[t], i };
-        m[A[i]] = i;
+    std::unordered_map<int, int> map;
+    for (int i = 0; i < array.size(); ++i)
+    {
+        int t = target - array[i];
+        if (map.count(t)) return { map[t], i };
+        map[array[i]] = i;
     }
-        return {};
+    return {};
 }
 
 int main()
 {
-    vector <int> a = {2, 7, 11, 15};
+    std::vector <int> array = { 2, 7, 11, 15 };
     int target = 9;
 
-    vector <int> result = twoSum(a, target);
-    
+    std::vector <int> result = twoSum(array, target);
+
+    std::cout << "Input: nums = ";
+
+    for (int i = 0; i < array.size(); i++)
+        std::cout << "[" << array[i] << "]";
+
+    std::cout << ", target = " << target << std::endl;
+
+    std::cout << "Output: ";
+
     for (int i = 0; i < result.size(); i++)
     {
-        cout << "["<<result[i] << "]";
+        std::cout << "[" << result[i] << "]";
     }
+
+    std::cin.get();
 }

e75e84da0a22e821be95cdb79450ffcfaa92ff87
Jonas Lara <adrianlr2515@outlook.com>
2022-11-21 19:46:11 -0600

    AddTwoNumbers C++ Completo

--- Algoritmos/02-AddTwoNumbers.cpp
+++ Algoritmos/02-AddTwoNumbers.cpp
@@ -44,6 +44,7 @@ ListNode* addTwoNumbers(ListNode* a, ListNode* b) {
 
 int main()
 {
+    //Construir primer lista enlazada
     ListNode* l1 = new ListNode();
     ListNode* l2 = new ListNode();
     ListNode* l3 = new ListNode();
@@ -57,9 +58,32 @@ int main()
     l3->val = 3;
     l3->next = NULL;
 
+    cout << "Input: l1 =  " ;
     printList(l1);
+    cout << ", ";
 
-    ListNode addTwoNumbers(l1);
+    //Construir segunda lista enlazada
+    ListNode* l4 = new ListNode();
+    ListNode* l5 = new ListNode();
+    ListNode* l6 = new ListNode();
+
+    l4->val = 5;
+    l4->next = l5;
+    l5->val = 6;
+    l5->next = l6;
+    l6->val = 4;
+    l6->next = NULL;
+
+    cout << "l2 =  " ;
+    printList(l4);
+    cout << "\n";
+
+    
+    ListNode* l7 = addTwoNumbers(l1, l4);
+
+    cout << "Output: ";
+    printList(l7);
+    cout << "\n";
 
     cin.get();
 
